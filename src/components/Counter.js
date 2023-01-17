a56bd431afa8b3c5f8c57dac84baf849fd6b7fca
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-17 21:24:39 +0600

    useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,33 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../redux/counter/action";
 
 export default function Counter() {
-    const [count, setCount] = useState(0);
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
 
-    const increment = () => {
-        setCount((prevCount) => prevCount + 1);
-    };
-
-    const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    };
-
-    return (
-        <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-            <div className="text-2xl font-semibold">{count}</div>
-            <div className="flex space-x-3">
-                <button
-                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    onClick={increment}
-                >
-                    Increment
-                </button>
-                <button
-                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    onClick={decrement}
-                >
-                    Decrement
-                </button>
-            </div>
-        </div>
-    );
+  const handleIncrement = (value) => {
+    dispatch(increment(value));
+  };
+  const handleDecrement = (value) => {
+    dispatch(decrement(value));
+  };
+  return (
+    <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+      <div className="text-2xl font-semibold">{count}</div>
+      <div className="flex space-x-3">
+        <button
+          onClick={() => handleIncrement(3)}
+          className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => handleDecrement(7)}
+          className="bg-red-400 text-white px-3 py-2 rounded shadow"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
